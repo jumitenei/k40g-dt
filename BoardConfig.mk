7497389fc49036003e5803743d37ad1bc92cfa54
kdj <3356985636@qq.com>
2024-03-03 21:17:30 +0800

    Git LFS commit

---
+++ BoardConfig.mk
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c66f7daf8eb696733cb10272e7aac7b67cb927c1fffed06fb58749f94f365774
+size 2195
---
+++ twrp_ares.mk
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf0556a88edc4187348e3495b766f4633fa37bc798f9db8523aa42ea6b02f03d
+size 628
